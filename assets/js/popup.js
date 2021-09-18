eeceba3d7990176c182303cbe76b479116813d17
todaybrian <todaybryan@gmail.com>
2021-09-18 19:00:09 -0400

    edits

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -12,6 +12,9 @@ function showSection(x){
     for(var i = 1; i <= 3; i++){
         if(i==x) continue;
         document.getElementById("section-" + i).style.display="none";
+        document.getElementById("showSection"+i).classList.remove("text-blue-700");
     }
     document.getElementById("section-" + x).style.display="block";
+    document.getElementById("showSection"+x).classList.add("text-blue-700");
+
 }
