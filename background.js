42e9b417411f49b04675243d5de74f27ee269172
Potatoritos <potatoritos@gmail.com>
2021-09-19 00:25:00 -0400

    Revamp background.js

--- background.js
+++ background.js
@@ -1,73 +1,57 @@
-function initBlockList() {
-    console.log("hi");
+var settings = {};
+var default_settings = {
+    blockReason: "reason",
+    isWhitelist: false,
+    softblockEnabled: false,
+    softblockPeriod: 300,
+    blockList: ["duckduckgo.com"],
+    blockEnabled: false
+};
 
-    var softblockEnabled;
+function initSettings() {
 
-    chrome.storage.sync.get({blockReason: false}, function(data) {
-        chrome.storage.sync.set({blockReason: data.blockReason}, function() {
-            console.log("blockReason saved: " + data.blockReason);
+    for (let [key, val] of Object.entries(default_settings)) {
+        chrome.storage.sync.get({[key]: val}, function(data) {
+            chrome.storage.sync.set({[key]: data[key]}, function() {
+                updateSetting(key, data[key]);
+            });
         });
-    });
-
-    chrome.storage.sync.get({isWhitelist: false}, function(data) {
-        chrome.storage.sync.set({isWhitelist: data.isWhitelist}, function() {
-            console.log("isWhitelist saved: " + data.isWhitelist);
-        });
-    });
-
-    chrome.storage.sync.get({softblockEnabled: false}, function(data) {
-        chrome.storage.sync.set({softblockEnabled: data.softblockEnabled}, function() {
-            console.log("softblockEnabled saved: " + data.softblockEnabled);
-            softblockEnabled = data.softblockEnabled;
-        });
-    });
-    
-    // the time in between each softblock reminder
-    chrome.storage.sync.get({softblockPeriod: 300000}, function(data) {
-        chrome.storage.sync.set({softblockEnabled: data.softblockPeriod}, function() {
-            console.log("softblockPeriod saved: " + data.softblockPeriod);
-        });
-    });
-
-    chrome.storage.sync.get({blockList: []}, function(data) {
-        // actual code
-        //chrome.storage.sync.set({blockList: data.blockList}, function() {
-          //  console.log("blockList saved");
-        //});
-        
-        // testing code
-        chrome.storage.sync.set({blockList: ['duckduckgo.com']}, function() {
-            console.log("blockList saved: ");
-            console.log(data.blockList);
-        });
-
-
-        //if (softblockEnabled) {
-        //for testing purposes:
-        if (false) {
-            updateListener(data.blockList);
-        }
-    });
+    }
 
 }
+
+function updateSetting(key, val) {
+    settings[key] = val;
+    console.log(`${key} changed: ${val}`);
+    if (((key === "blockList" && settings.blockEnabled) || (key === "blockEnabled" && val)) && !settings.softblockEnabled) {
+        console.log("enabling.");
+        updateListener(settings.blockList);
+    }
+    if ((key === "softblockEnabled" && val) || (key === "blockEnabled" && !val)) {
+        console.log("disabling.");
+        chrome.webRequest.onBeforeRequest.removeListener(redirect);
+    }
+}
+
+function checkUpdates(changes, namespace) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        updateSetting(key, newValue);
+    }
+}
+
+chrome.storage.onChanged.addListener(checkUpdates);
+
+chrome.runtime.onInstalled.addListener(function() {
+    initSettings();
+});
+
+chrome.runtime.onStartup.addListener(function() {
+    initSettings();
+});
 
 function urlIsWebsite(url) {
     return !url.startsWith("chrome://") && !url.startsWith("chrome-extension://") && !url.startsWith("about:");
 }
-
-chrome.runtime.onInstalled.addListener(function() {
-    initBlockList();
-//    chrome.storage.sync.set({ test });
-//    chrome.tabs.query({}, tabs => {
-//        tabs.forEach(tab => {
-//            console.log(tab.url);
-//        });
-//    });
-});
-
-chrome.runtime.onStartup.addListener(function() {
-    initBlockList();
-});
 
 function getCleanURL(url) {
     url = url.split('/')[2];
@@ -79,10 +63,6 @@ function getCleanURL(url) {
 
 function redirect(data) {
     var page = chrome.extension.getURL("pages/hardblock.html");
-    //console.log(data);
-    //chrome.storage.local.set({prevURL: data.url}, function() {
-    //    console.log("prevURL stored: " + data.url);
-    //})
     return {redirectUrl: page};
 }
 
@@ -99,7 +79,6 @@ function updateListener(urls) {
     urls.forEach(function(url) {
         patterns.push(urlToPattern(url));
     });
-    console.log("patterns: " + patterns);
 
     if (chrome.webRequest.onBeforeRequest.hasListener(redirect)) {
         chrome.webRequest.onBeforeRequest.removeListener(redirect);
@@ -108,43 +87,51 @@ function updateListener(urls) {
     chrome.webRequest.onBeforeRequest.addListener(redirect, {urls: patterns}, ["blocking"]);
 }
 
-var isLooping = false;
 
-function loopSoftblockTab() {
-    var b = false;
-    chrome.storage.sync.get({blockList: []}, function(data) {
+var doSoftLoop = false;
+var softLoopCnt = 0;
+
+function loop() {
+    if (doSoftLoop) {
+        softLoopCnt++;
+        console.log(`softLoopCnt: ${softLoopCnt}`);
+    } else {
+        softLoopCnt = -1;
+    }
+
+    if (softLoopCnt != -1 && softLoopCnt >= settings.softblockPeriod) {
+        softLoopCnt = 0;
+
         chrome.tabs.query({}, function(tabs) {
-            isLooping = false;
-            for (var i = 0; i < tabs.length; i++) {
-                if (urlIsWebsite(tabs[i].url)) {
-                
-                    var url = getCleanURL(tabs[i].url);
-
-
-                    if (data.blockList.includes(url)) {
-                        isLooping = true;
-                        chrome.tabs.query({}, function(tabs) {
-                            tabs.forEach(function(tab) {
-                                if (tab.url.startsWith("chrome-extension://") && tab.url.endsWith("/pages/softblock.html")) {
-                                    chrome.tabs.remove(tab.id, function() { });
-                                }
-                            });
-                        });
-                        chrome.tabs.create({ url: "pages/softblock.html" });
-                        break;
+            var b = true;
+            tabs.forEach(function(tab) {
+                if (urlIsWebsite(tab.url)) {
+                    var url = getCleanURL(tab.url);
+                    if (settings.blockList.includes(url)) {
+                        b = false;
                     }
-                }   
+                }
+            });
+            
+            if (b) {
+                doSoftLoop = false;
+                return;
             }
-        });
-    });
 
-    chrome.storage.sync.get({softblockPeriod: 300000}, function(data) {
-        setTimeout(loopSoftblockTab, data.softblockPeriod);
-    });
+            tabs.forEach(function(tab) {
+                if (tab.url.startsWith("chrome-extension://") && tab.url.endsWith("/pages/softblock.html")) {
+                    chrome.tabs.remove(tab.id, function() { });
+                }
+            });
+            chrome.tabs.create({ url: "pages/softblock.html" });
+        });
+    }
 }
 
+setInterval(loop, 1000);
+
 function openSoftblockTab() {
-    if (isLooping) return;
+    if (doSoftLoop) return;
     chrome.tabs.query({}, function(tabs) {
         tabs.forEach(function(tab) {
             if (tab.url.startsWith("chrome-extension://") && tab.url.endsWith("/pages/softblock.html")) {
@@ -153,11 +140,7 @@ function openSoftblockTab() {
         });
     });
     chrome.tabs.create({ url: "pages/softblock.html" });
-
-    isLooping = true;
-    chrome.storage.sync.get({softblockPeriod: 300000}, function(data) {
-        setTimeout(loopSoftblockTab, data.softblockPeriod);
-    });
+    doSoftLoop = true;
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
@@ -166,11 +149,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
         var url = getCleanURL(changeInfo.url);
 
-        chrome.storage.sync.get(['blockList'], function(data) {
-            if (data.blockList.includes(url)) {
-                openSoftblockTab();
-            }
-        });
+        if (settings.blockList.includes(url)) {
+            openSoftblockTab();
+        }
 
     }
 });
