4d1f983e99b31c491c3cba649897266e94b83f93
todaybrian <todaybryan@gmail.com>
2021-09-18 21:06:49 -0400

    finish blocked settings

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -6,8 +6,6 @@ for (var i = 1; i <= 3; i++) {
 
 }
 
-
-
 function showSection(x) {
     for (var i = 1; i <= 3; i++) {
         if (i == x) continue;
@@ -18,27 +16,42 @@ function showSection(x) {
     document.getElementById("showSection" + x).classList.add("text-blue-700");
 }
 
-if (localStorage.getItem("softblockEnabled") == undefined) localStorage.setItem("softblockEnabled", false);
 
-var isSoftEnabled = localStorage.getItem("softblockEnabled");
-var toggleSoft = document.getElementById("toggleSoftBlock"), toggleSoftButton = document.getElementById("toggleSoftBlockButton");
-toggleSoft.addEventListener('click', function () {
-    if (isSoftEnabled) {
-        isSoftEnabled = 0;
-        localStorage.setItem("softblockEnabled", false);
-        toggleSoft.classList.remove("bg-blue-600");
-        toggleSoft.classList.add("bg-gray-200");
-        toggleSoftButton.classList.add("translate-x-0");
-        toggleSoftButton.classList.remove("translate-x-5");
-        console.log(isSoftEnabled);
-    } else {
-        isSoftEnabled = 1;
-        localStorage.setItem("softblockEnabled", true);
+var isSoftEnabled;
+chrome.storage.sync.get("softblockEnabled", function(data) {
+    isSoftEnabled = data.softblockEnabled;
+    if(isSoftEnabled){
         toggleSoft.classList.add("bg-blue-600");
         toggleSoft.classList.remove("bg-gray-200");
         toggleSoftButton.classList.remove("translate-x-0");
         toggleSoftButton.classList.add("translate-x-5");
-        console.log(isSoftEnabled);
+    }
+});
+
+chrome.storage.sync.get("blockList", function(data) {
+    document.getElementById("blockedSitesTextArea").value = data.blockList.join('\n');
+});
+
+chrome.storage.sync.get("isWhitelist", function(data){
+    var opt = document.getElementById("bwSelect").children;
+    console.log(opt);
+    opt[0+(data.isWhitelist)].setAttribute("selected", "selected");
+});
+
+var toggleSoft = document.getElementById("toggleSoftBlock"), toggleSoftButton = document.getElementById("toggleSoftBlockButton");
+toggleSoft.addEventListener('click', function () {
+    if (isSoftEnabled) {
+        isSoftEnabled = 0;
+        toggleSoft.classList.remove("bg-blue-600");
+        toggleSoft.classList.add("bg-gray-200");
+        toggleSoftButton.classList.add("translate-x-0");
+        toggleSoftButton.classList.remove("translate-x-5");
+    } else {
+        isSoftEnabled = 1;
+        toggleSoft.classList.add("bg-blue-600");
+        toggleSoft.classList.remove("bg-gray-200");
+        toggleSoftButton.classList.remove("translate-x-0");
+        toggleSoftButton.classList.add("translate-x-5");
     }
 }, false);
 
@@ -47,8 +60,17 @@ function processForm(e) {
     var rawBlocks = document.getElementById("blockedSitesTextArea").value.split("\n");
     var newBlockedSites = rawBlocks.filter((a)=>a);
     chrome.storage.sync.set({blockList: newBlockedSites}, function() {
-        console.log("BlockList saved: " + data.blockList);
+        console.log("BlockList saved: ");
+        console.log(newBlockedSites);
     });
+    var isWhitelisted = document.getElementById("bwSelect").value==="Whitelisted";
+    chrome.storage.sync.set({isWhitelist: isWhitelisted}, function() {
+        console.log("isWhitelist saved: " + isWhitelisted);
+    });
+    chrome.storage.sync.set({softblockEnabled: isSoftEnabled}, function() {
+        console.log("softblockEnabled saved: " + isSoftEnabled);
+    });
+    document.getElementById("savedBlocked").classList.remove("hidden");
     return false;
 }
 var form = document.getElementById('submitBlockedSites');
