65d7c1fd58f016299f3af9b17d69a6fac3954ecf
todaybrian <todaybryan@gmail.com>
2021-09-18 22:52:24 -0400

    add eyes

--- pages/softblock.js
+++ pages/softblock.js
@@ -1,3 +1,19 @@
+var balls = document.getElementsByClassName("ball");
+				  document.onmousemove = function(){
+					var x = event.clientX * 100 / window.innerWidth + "%";
+					var y = event.clientY * 100 / window.innerHeight + "%";
+					//event.clientX => get the horizontal coordinate of the mouse
+					//event.clientY => get the Vertical coordinate of the mouse
+					//window.innerWidth => get the browser width
+					//window.innerHeight => get the browser height
+				
+					for(var i=0;i<2;i++){
+					  balls[i].style.left = x;
+					  balls[i].style.top = y;
+					  balls[i].style.transform = "translate(-"+x+",-"+y+")";
+					}
+				  }
+
 function getCleanURL(url) {
     url = url.split('/')[2];
     if (url.split('.')[0] === "www") {
