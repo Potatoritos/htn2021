50218dc1a66b066064a5061cb651192abe1aa258
todaybrian <todaybryan@gmail.com>
2021-09-19 00:42:39 -0400

    I got sign in to work

---
+++ assets/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBcHlo-LYBKkczJpCQNQB2cMOLQE_rxgRQ",
+    authDomain: "lighthouse-a007c.firebaseapp.com",
+    databaseURL: "https://lighthouse-a007c-default-rtdb.firebaseio.com",
+    projectId: "lighthouse-a007c",
+    storageBucket: "lighthouse-a007c.appspot.com",
+    messagingSenderId: "608101274125",
+    appId: "1:608101274125:web:5920919e909739de6abb44",
+    measurementId: "G-5E10K6JYS7"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  
+  const 
